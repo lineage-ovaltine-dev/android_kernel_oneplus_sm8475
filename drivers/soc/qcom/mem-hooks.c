000191fd6768366d9eb5afcfc1b80db37d249039
pppig236 <priv@pppig236.com>
2023-11-09 08:48:34 -0500

    Add drivers/soc/ modifications
    Signed-off-by: pppig236 <priv@pppig236.com>

--- drivers/soc/qcom/mem-hooks.c
+++ drivers/soc/qcom/mem-hooks.c
@@ -15,6 +15,7 @@
 static unsigned long panic_on_oom_timeout;
 struct task_struct *saved_tsk;
 
+
 #define PANIC_ON_OOM_DEFER_TIMEOUT (5*HZ)
 
 
@@ -39,12 +40,12 @@ static void set_swap_cache(void *data, gfp_t *flag)
 	*flag |= __GFP_CMA;
 }
 
-static void reap_eligible(void *data, struct task_struct *task, bool *reap)
-{
-	/* TODO: Can this logic be moved to module params approach? */
-	if (!strcmp(task->comm, "lmkd") || !strcmp(task->comm, "PreKillActionT"))
-		*reap = true;
-}
+//static void reap_eligible(void *data, struct task_struct *task, bool *reap)
+//{
+//	/* TODO: Can this logic be moved to module params approach? */
+//	if (!strcmp(task->comm, "lmkd") || !strcmp(task->comm, "athena_killer") || !strcmp(task->comm, "PreKillActionT"))
+//		*reap = true;
+//}
 
 static void __oom_panic_defer(void *data, struct oom_control *oc, int *val)
 {
@@ -78,7 +79,22 @@ out:
 
 static void balance_reclaim(void *unused, bool *balance_anon_file_reclaim)
 {
-	*balance_anon_file_reclaim = true;
+	pg_data_t *pgdat;
+	struct zone *zone;
+	unsigned long free_pages_threshold = 0;
+	unsigned long normal_zone_free_pages = 0;
+
+	pgdat = NODE_DATA(0);
+	zone = &pgdat->node_zones[ZONE_NORMAL];
+	free_pages_threshold = low_wmark_pages(zone) + ((high_wmark_pages(zone)-low_wmark_pages(zone)) >> 1);
+
+	/*we do not balance reclaim anon and page cache files for reclaim when free < low + (high - low)/2;*/
+	normal_zone_free_pages = zone_page_state(zone, NR_FREE_PAGES);
+	if(normal_zone_free_pages <  free_pages_threshold) {
+		*balance_anon_file_reclaim = false;
+	} else {
+		*balance_anon_file_reclaim = true;
+	}
 }
 
 static void allow_subpage_alloc(void *data, bool *allow_subpage_alloc, struct device *dev,
@@ -98,12 +114,6 @@ static void allow_subpage_alloc(void *data, bool *allow_subpage_alloc, struct de
 		*allow_subpage_alloc = true;
 		*size = PAGE_ALIGN(*size);
 	}
-}
-
-static void allow_shared_pages_reclaim(void *unused, struct vm_area_struct *vma, bool *allow_shared)
-{
-	/* Allow shared pages reclaim through process_madvise() only */
-	*allow_shared = (current->mm != vma->vm_mm);
 }
 
 static int __init init_mem_hooks(void)
@@ -128,11 +138,11 @@ static int __init init_mem_hooks(void)
 		return ret;
 	}
 
-	ret = register_trace_android_vh_process_killed(reap_eligible, NULL);
-	if (ret) {
-		pr_err("Failed to register process_killed hooks\n");
-		return ret;
-	}
+	//ret = register_trace_android_vh_process_killed(reap_eligible, NULL);
+	//if (ret) {
+	//	pr_err("Failed to register process_killed hooks\n");
+	//	return ret;
+	//}
 
 	ret = register_trace_android_vh_oom_check_panic(__oom_panic_defer,
 							NULL);
@@ -156,17 +166,18 @@ static int __init init_mem_hooks(void)
 		return ret;
 	}
 
-	ret = register_trace_android_vh_madvise_cold_or_pageout(
-				allow_shared_pages_reclaim, NULL);
-	if (ret) {
-		pr_err("Failed to register madvise_cold_or_pageout_pte_range hook\n");
-		return ret;
-	}
 
 	return 0;
 }
 
+void exit_mem_hooks(void)
+{
+	unregister_trace_android_vh_oom_check_panic(
+			__oom_panic_defer, NULL);
+}
+
 module_init(init_mem_hooks);
+module_exit(exit_mem_hooks);
 
 MODULE_DESCRIPTION("Qualcomm Technologies, Inc. Memory Trace Hook Call-Back Registration");
 MODULE_LICENSE("GPL v2");
